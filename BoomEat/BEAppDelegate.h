ccc8ea1b5c253f5eb9552b2d253c58e6836ef67c
jun <jun@jun-mac.local>
2015-07-01 22:08:09 +0800

    first commit

---
+++ BoomEat/BEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BEAppDelegate.h
+//  BoomEat
+//
+//  Created by jun on 15-7-1.
+//  Copyright (c) 2015年 com.be. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BoomEat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BoomEat
+//
+//  Created by jun on 15-7-1.
+//  Copyright (c) 2015年 com.be. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BEAppDelegate class]));
+    }
+}
